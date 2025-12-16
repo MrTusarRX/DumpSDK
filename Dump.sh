8a31f238203122746feb6d7fb6e944aac34caf0b
tusar khan <114900895+MrTusarRX@users.noreply.github.com>
2025-12-16 17:10:16 +0530

    initial commit
    just made everything as shortcut

--- Dump.sh
+++ Dump.sh
@@ -14,7 +14,5 @@ echo "
 command -v python3 >/dev/null 2>&1 || apt install -y python3
 command -v git >/dev/null 2>&1 || pkg install -y git
 python3 -c "import tqdm" >/dev/null 2>&1 || pip install tqdm
-
-cd DumpSDK || exit 1
 python3 Dump.py
 
